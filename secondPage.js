f0f4fb88ac3c2eae1cec2946dba942eb904a88fe
Alexandre <alexandrefsoficial@gmail.com>
2022-11-02 14:29:38 -0300

    select Answer implemented

--- secondPage.js
+++ secondPage.js
@@ -1,3 +1,6 @@
+let rightAnswers = 0;
+let wrongAnswers = 0;
+let NumberOfQuestions = 0;
 function getQuizz(id){
     const quizz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/"+id)
     quizz.then(buildQuizz);
@@ -17,12 +20,13 @@ function buildQuizz(response){
     `
     //building questions
     const questionsSection = document.querySelector("section") 
+    NumberOfQuestions = response.data.questions.length;
     for(i=0;i<response.data.questions.length;i++){
         let question = response.data.questions[i];
         let answers = "";
         for(j=0;j<question.answers.length;j++){
             answers += `
-            <div class="answer ${question.answers[j].isCorrectAnswer}">
+            <div class="answer ${question.answers[j].isCorrectAnswer} answerNotChosen" onclick="selectAnswer(this)">
                 <img src="${question.answers[j].image}" alt="answer image">
                 <p>
                     ${question.answers[j].text}
@@ -37,6 +41,7 @@ function buildQuizz(response){
             </div>
             <div class="questionAnswers">
                 ${answers}
+                <div class="select hidden"></div>
             </div>
         </div>
         `
@@ -44,6 +49,30 @@ function buildQuizz(response){
         let color = document.getElementById(`${i}`);
         color.style.backgroundColor = question.color;
     }
+}
+function selectAnswer(element){
+    console.log("entrou")
+    const parent = element.parentNode
+    element.classList.add("selected")
+    if(element.classList.contains("true")){
+        rightAnswers++
+    }else{
+        wrongAnswers++
+    }
+    for(i=0;i<parent.children.length;i++){
+        let option = parent.children[i]
+        if(option.classList.contains("select")){
+            option.classList.remove("hidden")
+            break;
+        }else{
+            option.classList.remove("answerNotChosen")
+        }
+    }
+    if((rightAnswers+wrongAnswers) === NumberOfQuestions){
+        finishGame()   
+    }
+    console.log(rightAnswers)
+    console.log(wrongAnswers)
 }
 
 const urlParams = new URLSearchParams(window.location.search);
