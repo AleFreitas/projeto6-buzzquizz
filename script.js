490d4ce484300f7fdd27944fbd16226658fd4724
Eduardo Souza <edcampossouza@gmail.com>
2022-11-02 13:02:03 -0300

    implemented: user quizzes layout

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 function fetchQuizzes() {
-  const quizzesElement = document.querySelector(".quizzes");
+  const quizzesElement = document.querySelector(".quizzes-panel .quizzes");
+  const localQuizzesElement = document.querySelector(".quizzes");
+  const localQuizzesIds = getLocalStorageIDs();
+  let hasLocalQuizzes = false;
+  console.log(localQuizzesIds);
   axios
     .get(URL_QUIZZES)
     .then(function (response) {
-      const elements = response.data.map(
-        (quizz) =>
-          `<div onclick="beginQuizz(${quizz.id})" class="quizz-card"> <img src="${quizz.image}"></img> <div class="title">${quizz.title}</div> </div>`
-      );
-      quizzesElement.innerHTML = elements.join("");
+      response.data.forEach((quizz) => {
+        const inner = `<div onclick="beginQuizz(${quizz.id})" class="quizz-card"> <img src="${quizz.image}"></img> <div class="title">${quizz.title}</div> </div>`;
+        if (localQuizzesIds.find((id) => id == quizz.id)) {
+          console.log(inner);
+          localQuizzesElement.innerHTML += inner;
+          console.log(localQuizzesElement);
+        } else {
+          quizzesElement.innerHTML += inner;
+          hasLocalQuizzes = true;
+        }
+      });
       quizzes = response.data;
+      if (hasLocalQuizzes) {
+        const userQuizzes = document.querySelector(".user-quizzes");
+        userQuizzes.classList.remove("empty");
+        userQuizzes.classList.add("not-empty");
+      }
     })
     .catch(function (err) {
       console.log(err);
