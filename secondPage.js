1759d9f0cc5bcf2b6d35e98d1d9ba72fcd6bfd33
Alexandre <alexandrefsoficial@gmail.com>
2022-11-02 13:40:19 -0300

    secondPage get and show methods implemented

--- secondPage.js
+++ secondPage.js
@@ -7,15 +7,49 @@ function getQuizz(id){
       });
 }
 function buildHeader(response){
-    console.log("buildingHeader")
-    header = document.querySelector(".quizzHeader")
+    //building header
+    const header = document.querySelector(".quizzHeader")
     header.innerHTML = `
     <img src="${response.data.image}">
     <div>
         <p>${response.data.title}</p>
     </div>
     `
+    buildQuestions(response)
 }
+function buildQuestions(response){
+    //building questions
+    const questionsSection = document.querySelector("section") 
+    for(i=0;i<response.data.questions.length;i++){
+        let question = response.data.questions[i];
+        let answers = "";
+        for(j=0;j<question.answers.length;j++){
+            answers += `
+            <div class="answer ${question.answers[j].isCorrectAnswer}">
+                <img src="${question.answers[j].image}" alt="answer image">
+                <p>
+                    ${question.answers[j].text}
+                </p>
+            </div>
+            `
+        }
+        newQuestion = `
+        <div class="question">
+            <div class="questionTitle" id="${i}">
+                ${question.title}
+            </div>
+            <div class="questionAnswers">
+                ${answers}
+            </div>
+        </div>
+        `
+        questionsSection.innerHTML += newQuestion
+        let color = document.getElementById(`${i}`);
+        color.style.backgroundColor = question.color;
+    }
+
+}
+
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("quizzID");
 getQuizz(id)
