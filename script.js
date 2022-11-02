4c1da5fe9bb5cc29ee5c3bfb1dc9332868d702c9
Eduardo Souza <edcampossouza@gmail.com>
2022-11-02 08:57:12 -0300

    added beginQuizz function to select a quizz

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const URL_QUIZZES = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 
+let selectedQuizz = null;
+let quizzes = null;
+
 function fetchQuizzes() {
   const quizzesElement = document.querySelector(".quizzes");
   axios
@@ -7,9 +10,10 @@ function fetchQuizzes() {
     .then(function (response) {
       const elements = response.data.map(
         (quizz) =>
-          `<div class="quizz-card"> <img src="${quizz.image}"></img> <div class="title">${quizz.title}</div> </div>`
+          `<div onclick="beginQuizz(${quizz.id})" class="quizz-card"> <img src="${quizz.image}"></img> <div class="title">${quizz.title}</div> </div>`
       );
       quizzesElement.innerHTML = elements.join("");
+      quizzes = response.data;
     })
     .catch(function (err) {
       console.log(err);
@@ -19,6 +23,17 @@ function fetchQuizzes() {
 
 function beginCreateQuizz() {
   window.location.href = "thirdPage.html";
+}
+
+function beginQuizz(quizzID) {
+  const arr = quizzes.filter(function (q) {
+    return q.id === quizzID;
+  });
+
+  if (arr.length >= 1) {
+    selectedQuizz = arr[0];
+    console.log(selectedQuizz);
+  }
 }
 
 fetchQuizzes();
